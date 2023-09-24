a08f69106c04e7cbeb33b0892cd48c148566b182
dar-rojas <dar.rojas@duocuc.cl>
2023-09-24 21:40:13 +0000

    limpiar, eliminar id y cargar datos de tablas dimensionales

---
+++ SQL/source_queries.sql
@@ -0,0 +1,49 @@
+/*
+	Consultas para extraer datos que se insertarán en el modelo estrella
+*/
+use Albarran;
+
+--DTiempo
+select 
+	YEAR(fecha) as anio,
+	MONTH(fecha) as mes,
+	DAY(fecha) as dia
+from ventas;
+
+--DUbicacion
+select 
+	s.sucursal as nomSucursal,
+	c.comuna as nomComuna
+from ventas v
+	join sucursales s on v.sucursal_id = s.sucursal_id
+	join comunas c on c.comuna_id = s.sucursal_id;
+
+--DProducto
+select 
+	p.nombre as nomProducto,
+	c.categoria as catProducto,
+	dv.cantidad as cantidad,
+	p.precio as precio,
+	dv.descuento as descuento
+from ventas v
+	join detalle_ventas dv on v.venta_id = dv.venta_id
+	join productos p on dv.producto_id = p.producto_id
+	join categorias c on p.categoria_id = c.categoria_id;
+
+--DCliente
+select 
+	c.nombre + ' '  + c.appaterno + ' ' + c.apmaterno as nombre_completo,
+	c.sexo as sexo,
+	c.fecha_nacimiento as fecha_nacimiento,
+	c.estado_civil as estado_civil
+from ventas v join clientes c on v.cliente_id = c.cliente_id;
+
+--DEmpleado
+select 
+	e.nombre + ' '  + e.appaterno + ' ' + e.apmaterno as nombre_completo,
+	e.fecha_contrato as fecha_contrato,
+	e.sueldo_base as sueldo_base,
+	c.nombre as cargo
+from ventas v
+	join empleados e on v.vendedor_id = e.empleado_id
+	join cargos c on c.cargo_id = e.cargo_id;
